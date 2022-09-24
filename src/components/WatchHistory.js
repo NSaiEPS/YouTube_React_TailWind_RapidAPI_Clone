97042155dee8ecd9de0125625648e8c5ba120a7d
NSaiEPS <sainizameps@gmail.com>
2022-09-24 15:43:57 +0530

    pareInt in watch history

--- src/components/WatchHistory.js
+++ src/components/WatchHistory.js
@@ -71,18 +71,18 @@ const WatchHistory = ({watchedVideoid,watchId,originalDate}) => {
         let getReqYear=date.toLocaleDateString().split('/')[2]
         console.log(getReqMonth,reqMonthpost)
     
-        if(getReqYear===reqYearpost){
+        if(parseInt(getReqYear)===parseInt(reqYearpost)){
             // Same year
-            if(getReqMonth=== reqMonthpost){
+            if(parseInt(getReqMonth)===parseInt(reqMonthpost)){
                 // same month
     
-                if(getReqDate===reqDatepost){
+                if(parseInt(getReqDate)===parseInt(reqDatepost)){
                     return 'today '
                 }
     
                 else {
     
-                    if( getReqDate- reqDatepost ===1 ){
+                    if( parseInt(getReqDate)- parseInt(reqDatepost) ===1 ){
                         return 'yesterday '
                     }
     
@@ -93,7 +93,7 @@ const WatchHistory = ({watchedVideoid,watchId,originalDate}) => {
             }
     
             else {
-                if( getReqMonth- reqMonthpost=== 1) {
+                if( parseInt(getReqMonth)- parseInt(reqMonthpost)=== 1) {
                 return 'last month'}
     
                 else {
@@ -104,7 +104,7 @@ const WatchHistory = ({watchedVideoid,watchId,originalDate}) => {
     
         else {
             // Different year
-            if(getReqYear ===reqYearpost -1){
+            if(parseInt(getReqYear) ===parseInt(reqYearpost) -1){
                 return 'a year back'
             }
             else return `${getReqYear-reqYearpost } years back`
